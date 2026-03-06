bc1e62c54162fa4dd8112deb121e8534d9b4c206
github-action-benchmark <github@users.noreply.github.com>
2026-03-06 14:54:04 +0000

    add Benchmark (cargo) benchmark result for 8779cc4f3551d8cea7bf0d20cdab820bccc9da54

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772808312320,
+  "lastUpdate": 1772808844416,
   "repoUrl": "https://github.com/yen0304/xlex",
   "entries": {
     "Benchmark": [
@@ -112,6 +112,120 @@ window.BENCHMARK_DATA = {
           {
             "name": "lazy_stream_rows_10k",
             "value": 8873100,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yen0304@users.noreply.github.com",
+            "name": "yen0304",
+            "username": "yen0304"
+          },
+          "committer": {
+            "email": "yen0304@users.noreply.github.com",
+            "name": "yen0304",
+            "username": "yen0304"
+          },
+          "distinct": true,
+          "id": "8779cc4f3551d8cea7bf0d20cdab820bccc9da54",
+          "message": "ci: remove debug step from benchmark job",
+          "timestamp": "2026-03-06T22:48:25+08:00",
+          "tree_id": "0df0acc33ce382789550895f0a911a497b2b6d7a",
+          "url": "https://github.com/yen0304/xlex/commit/8779cc4f3551d8cea7bf0d20cdab820bccc9da54"
+        },
+        "date": 1772808844118,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "workbook_open_1k_rows",
+            "value": 1391700,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "sheet_list",
+            "value": 11,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cell_get_single",
+            "value": 40,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cell_set_single",
+            "value": 214,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "row_append/100",
+            "value": 72618,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "row_append/1000",
+            "value": 193780,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "row_append/10000",
+            "value": 1305900,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "range_read_100_cells",
+            "value": 1293,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "workbook_save/100",
+            "value": 485880,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "workbook_save/1000",
+            "value": 2650100,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "workbook_save/5000",
+            "value": 12815000,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cell_ref_parse_simple",
+            "value": 25,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "cell_ref_parse_complex",
+            "value": 38,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "range_parse",
+            "value": 74,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "lazy_stream_rows_10k",
+            "value": 8855700,
             "range": "± 0",
             "unit": "ns/iter"
           }
